394e2c39c6af9d1a05fa3aaf8731acb23b663f23
ValentinaHeredia <112986490+ValentinaHeredia@users.noreply.github.com>
2024-11-10 19:59:33 -0300

    Add files via upload

---
+++ prograIII/src/test/java/com/tpo/prograIII/PrograIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpo.prograIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrograIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
